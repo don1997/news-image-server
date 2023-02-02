1245d8fc8040a6d353f0cdf1612192673c4f91df
don1997 <donald_j97@hotmail.com>
2023-02-01 22:04:41 -0500

    init 4

--- src/server.js
+++ src/server.js
@@ -43,6 +43,6 @@ app.get('/news', (req, res) => {
 });
 const port = process.env.PORT || 5000;
 
-app.listen(5000, () => {
+app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
